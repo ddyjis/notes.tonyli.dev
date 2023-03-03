d9e0e4600f1d83451a30413f92080d8d76e47bce
ddyjis <ddyjis@gmail.com>
2023-03-02 21:56:42 -0500

    Change Nextra settings

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,44 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import Logo from "public/pensieve.png";
+
+import Image from "next/image";
+import React from "react";
+import type { DocsThemeConfig } from "nextra-theme-docs";
+import { useConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  docsRepositoryBase: "https://github.com/ddyjis/notes.tonyli.dev",
+  editLink: {
+    text: "Edit",
+  },
+  head: () => {
+    const { frontMatter } = useConfig();
+    return (
+      <head>
+        <title>
+          {frontMatter.title ? `${frontMatter.title} | Pensieve` : "Pensieve"}
+        </title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </head>
+    );
+  },
+  logo: (
+    <>
+      <Image src={Logo} alt="Pensieve" width={36} height={36} />
+      <h1
+        style={{
+          marginLeft: "1rem",
+          fontSize: "1.25rem",
+          fontWeight: 700,
+        }}
+      >
+        Pensieve 儲思盆
+      </h1>
+    </>
+  ),
+  navigation: false,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/ddyjis/notes.tonyli.dev",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+};
 
-export default config
+export default config;
