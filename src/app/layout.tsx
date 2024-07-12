ee7a73e656fe59a098a784606e8f3b1c6dff9cd5
ddyjis <ddyjis@gmail.com>
2024-07-12 15:16:51 -0400

    feat: set up Next.js

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "@/app/global.css";
+import type { Metadata } from "next";
+import { Fira_Code } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+export const metadata = {
+  title: "__tony_li__",
+} satisfies Metadata;
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body className={cn("h-screen antialiased font-mono", font.variable)}>
+        {children}
+      </body>
+    </html>
+  );
+};
+export default Layout;
+const font = Fira_Code({ subsets: ["latin"], variable: "--font" });
