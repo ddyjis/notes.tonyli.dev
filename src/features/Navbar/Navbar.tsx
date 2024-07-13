04c192d172d51b861f5e26816f4b857a64a27b15
ddyjis <ddyjis@gmail.com>
2024-07-13 01:39:49 -0400

    fix: update styles in Navbar

--- src/features/Navbar/Navbar.tsx
+++ src/features/Navbar/Navbar.tsx
@@ -4,8 +4,8 @@ import NextLink from 'next/link'
 import logo from '@/app/pensieve.png'
 
 export const Navbar = () => (
-  <nav className='border-b-2 py-4'>
-    <div className='container'>
+  <nav className='sticky top-0 z-50 border-b-2 bg-white py-4'>
+    <div className='container max-w-xl'>
       <NextLink href='/'>
         <NextImage src={logo} alt='Index' height='32' />
       </NextLink>
