65a07d9b3b5196a167deb750cabe31f9ff713493
ddyjis <ddyjis@gmail.com>
2024-07-17 01:45:06 -0400

    feat: update frontend element

--- src/features/Note/MdxComponent.tsx
+++ src/features/Note/MdxComponent.tsx
@@ -4,6 +4,7 @@ import {type MDXContentProps, getMDXComponent} from 'mdx-bundler/client'
 import NextLink from 'next/link'
 import {useMemo} from 'react'
 
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from '@/components/ui/table'
 import {cn} from '@/lib/utils'
 
 const COMPONENTS = {
@@ -20,8 +21,26 @@ const COMPONENTS = {
     <p {...props} className={cn(className, 'mt-2 mb-2 text-base leading-snug')} />
   ),
   blockquote: ({className, ...props}) => (
-    <blockquote {...props} className={cn(className, 'mt-2 mb-2 border-l-2 py-1 pl-2 text-base')} />
+    <blockquote
+      {...props}
+      className={cn(className, '-ml-2 mt-2 mb-2 border-l-2 py-1 pl-2 text-base')}
+    />
   ),
+  ul: ({className, ...props}) => (
+    <ul {...props} className={cn(className, 'mt-2 mb-2 list-disc pl-5 text-base')} />
+  ),
+  ol: ({className, ...props}) => (
+    <ol {...props} className={cn(className, 'mt-2 mb-2 list-decimal pl-5 text-base')} />
+  ),
+  li: ({className, ...props}) => (
+    <li {...props} className={cn(className, 'mt-2 mb-2 text-base leading-snug')} />
+  ),
+  table: Table,
+  thead: TableHeader,
+  tbody: TableBody,
+  tr: TableRow,
+  th: TableHead,
+  td: TableCell,
   hashtag: ({value}) => (
     <NextLink href={`/_/tags/${value}`} className='font-semibold text-primary'>
       #{value}
