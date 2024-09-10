81c8ab14ddffbdca135b867bb9bf4d73120e2ca3
ddyjis <ddyjis@gmail.com>
2024-09-10 17:46:39 -0400

    feat: remove old algolia objects

--- src/lib/algolia.ts
+++ src/lib/algolia.ts
@@ -10,8 +10,16 @@ export const updateAlgoliaIndex = async (notesMetadata: Metadata['notes']) => {
     document,
   }))
   try {
+    const {hits} = await index.search('', {attributesToRetrieve: ['objectID']})
+    const newObjectIds = new Set(objects.map((obj) => obj.objectID))
+    const objectIdsToDelete = hits.map((hit) => hit.objectID).filter((id) => !newObjectIds.has(id))
+
     const result = await index.saveObjects(objects)
     console.log(`Updated ${result.objectIDs.length} records in Algolia index`)
+    if (objectIdsToDelete.length > 0) {
+      const result = await index.deleteObjects(objectIdsToDelete)
+      console.log(`Deleted ${result.objectIDs.length} records from Algolia index`)
+    }
   } catch (error) {
     console.error(`Error updating Algolia index: ${error}`)
   }
